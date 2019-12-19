b38cfaf00bdbd91adef8209d17a7bebc79409c9d
Phil Ma <phillio411@gmail.com>
2019-12-19 15:47:59 -0500

    add more target blanks to project links

--- src/components/Main/Projects/index.jsx
+++ src/components/Main/Projects/index.jsx
@@ -6,9 +6,9 @@ function Projects (props) {
     return (
         <div className="project-container" id={`project-${props.indexNum}`}>
             <h3>{props.project.name}</h3>
-            <h5 className="project-link"><a className="project-link" href={props.project.link} >Link: {props.project.link}</a></h5>
+            <h5 className="project-link"><a className="project-link" href={props.project.link} rel="noopener noreferrer" target="_blank">Link: {props.project.link}</a></h5>
             <img alt={props.project.name} src={props.project.pic} ></img>
-            <h5 className="project-link"><a className="project-link" href={props.project.repo}>Github: {props.project.repo}</a></h5>
+            <h5 className="project-link"><a className="project-link" href={props.project.repo} rel="noopener noreferrer" target="_blank">Github: {props.project.repo}</a></h5>
         </div>
     )
 }
