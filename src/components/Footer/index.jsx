e8c790c2a66b27e038efead66746bc0bda55f790
Phil Ma <phillio411@gmail.com>
2019-12-19 15:46:55 -0500

    add resume link and add target blank to links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,7 +8,9 @@ function Footer(props) {
       <div className="footer-box">
         <p className="footer-box-text">Phil Ma's<br></br>Contact Info</p>
         <p className="footer-box-text">Email:<br></br>Phillio411@gmail.com</p>
-        <p className="footer-box-text">LinkedIn:<br></br>www.linkedin.com/in/phil-ma</p>
+        <a className="footer-box-text" href="https://www.linkedin.com/in/phil-ma" rel="noopener noreferrer" target="_blank">LinkedIn:<br></br>www.linkedin.com/in/phil-ma</a>
+        <a className="footer-box-text" href="https://drive.google.com/file/d/14qMmg60oxnPy9CbMKwzp-M6D5RZcvVdZ/view?usp=sharing" rel="noopener noreferrer" target="_blank">Resume:<br></br>RESUME</a>
+        <p></p>
       </div>
     </footer>
   );
